557adc657fad9e313a102f8882a41ab9d3cd6102
Celestial24 <email_ng_celestial24@gmail.com>
2026-01-17 11:51:19 +0800

    .

--- Super-admin/auth/login.php
+++ Super-admin/auth/login.php
@@ -169,7 +169,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ATIERA Super Admin Login</title>
-    <link rel="icon" type="image/x-icon" href="../assets/image/logo2.png">
+    <link rel="icon" type="image/x-icon" href="../../assets/image/logo2.png">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
