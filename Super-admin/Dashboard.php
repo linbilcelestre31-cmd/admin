64a10d4ece586ae5c0bec9ca0dbb82a4a80cb52a
Celestial24 <email_ng_celestial24@gmail.com>
2026-01-17 11:46:33 +0800

    .

--- Super-admin/Dashboard.php
+++ Super-admin/Dashboard.php
@@ -67,6 +67,7 @@ $modules = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Super Admin Command Center | ATIERA</title>
+    <link rel="icon" type="image/x-icon" href="../assets/image/logo2.png">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
