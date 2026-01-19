0bae5d104b10f46a32842a1ed70c3de6af97d666
Celestial24 <email_ng_celestial24@gmail.com>
2026-01-19 22:09:12 +0800

    Fix 404 error by correcting SSO target paths in gateway.php

--- Super-admin/gateway.php
+++ Super-admin/gateway.php
@@ -69,12 +69,12 @@ $tokenData = [
 ];
 $token = base64_encode(json_encode($tokenData));
 
-// 5. I-define ang Target SSO URLs (Base sa iyong request)
+// 5. I-define ang Target SSO URLs (Corrected Paths)
 $sso_urls = [
-    'HR1' => 'https://hr1.atierahotelandrestaurant.com/hr1/sso-login.php',
-    'HR2' => 'https://hr2.atierahotelandrestaurant.com/hr2/sso-login.php',
-    'HR3' => 'https://hr3.atierahotelandrestaurant.com/hr3/sso-login.php',
-    'HR4' => 'https://hr4.atierahotelandrestaurant.com/hr4/sso-login.php',
+    'HR1' => 'https://hr1.atierahotelandrestaurant.com/sso-login.php',
+    'HR2' => 'https://hr2.atierahotelandrestaurant.com/sso-login.php',
+    'HR3' => 'https://hr3.atierahotelandrestaurant.com/sso-login.php',
+    'HR4' => 'https://hr4.atierahotelandrestaurant.com/sso-login.php',
     'CORE1' => 'https://core1.atierahotelandrestaurant.com/sso-login.php',
     'CORE2' => 'https://core2.atierahotelandrestaurant.com/sso-login.php'
 ];
