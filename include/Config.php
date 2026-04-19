f8e5903f6d4a86b100dc7f0cf3288ab3ebb0888d
linbil24 <linbilcelestre3@gmail.com>
2026-04-20 03:14:26 +0800

    .

--- include/Config.php
+++ include/Config.php
@@ -3,10 +3,11 @@
 // Change these values to use a different sender email account
 
 define('SMTP_HOST', 'smtp.gmail.com');
-
 define('SMTP_PORT', 587);
-define('SMTP_USER', 'atiera41001@gmail.com'); // Put your random email here
-define('SMTP_PASS', 'sqbhijukobzglnwk');    // Put your random email App Password here
+define('SMTP_SECURE', 'tls');
+
+define('SMTP_USER', 'atiera41001@gmail.com');
+define('SMTP_PASS', 'sqbhijukobzglnwk');
 define('SMTP_FROM_EMAIL', 'atiera41001@gmail.com');
 define('SMTP_FROM_NAME', 'ATIERA Hotel');
 
@@ -17,7 +18,6 @@ function getBaseUrl()
     $host = $_SERVER['HTTP_HOST'];
     $currentDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
     // Go up levels until we reach the 'admin' or project root
-    // For admin.atierahotelandrestaurant.com/admin/include/Settings.php, we want /admin/
     $parts = explode('/', trim($currentDir, '/'));
     if (in_array('include', $parts)) {
         $projectRoot = '/' . implode('/', array_slice($parts, 0, array_search('include', $parts)));
@@ -26,6 +26,6 @@ function getBaseUrl()
     } else {
         $projectRoot = $currentDir;
     }
-    return $protocol . "://" . $host . rtrim($projectRoot, '/');
+    return $protocol . "://" . $host . '/' . trim($projectRoot, '/') . '/';
 }
 ?>
